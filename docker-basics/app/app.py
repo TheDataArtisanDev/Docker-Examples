b0bb1f51a4ebbd5a7a0e67324e46ab284a1c7f1d
Yaseen Banu <ybanu123@gmail.com>
2025-05-18 00:35:40 +0530

    Basic docker app

--- docker-basics/app/app.py
+++ docker-basics/app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello from Docker!'
+    return 'Hello from Docker-1!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
